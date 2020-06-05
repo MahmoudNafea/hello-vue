c6e4d21a015443129a1011a041824ebf14387df8
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-06-05 23:51:37 +0800

    navbar and routing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Vue</p>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "About" // eslint-disable-line
+}; // eslint-disable-line
+</script>
