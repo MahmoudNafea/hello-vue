df6d3070044f925bb39ace5aa041829e144784cb
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-06-06 21:16:14 +0800

    directives and input handling

---
+++ src/components/AddItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>This's add item component</h2>
+    <li>{{items.text}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddItem",
+  props: ["items"],
+  data() {
+    return {
+      groceryList: [
+        { id: 0, text: "veg" },
+        { id: 1, text: "veg" },
+        { id: 2, text: "veg" }
+      ]
+    };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,34 @@
-<template>
-  <div>
+<template   >
+  <div class="container">
     <h2>Home</h2>
-    <p>{{message}}</p>
+    <!-- {{message}} -->
+    <span v-bind:title="message">Hover over</span>
+    <br />
+    <span v-if="seen">See more</span>
+    <form>
+      <input type="text" placeholder="Add something" />
+      <button v-on:click="addItemHandler">Add Item</button>
+    </form>
+    <ol>
+      <AddItem v-for="item in groceryList" v-bind:item="item" v-bind:key="item.key"></AddItem>
+    </ol>
   </div>
 </template>
 
 <script>
+import AddItem from "./../components/AddItem";
+
 export default {
   name: "Home",
-  data: { message: "Hi" }
+  components: { AddItem },
+  data() {
+    return { message: "Hi", seen: true };
+  },
+  methods: {
+    addItemHandler(e) {
+      e.preventDefault();
+      console.log("added");
+    }
+  }
 };
 </script>
